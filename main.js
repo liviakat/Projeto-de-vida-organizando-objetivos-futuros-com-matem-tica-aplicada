492d5dd1d4ae755bcd11c10f90fc5692555fc386
Livia <108752437+liviakat@users.noreply.github.com>
2024-04-11 15:53:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
+       
     for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = calculaTempo(tempos[i]);   
     }
@@ -52,4 +53,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-comecaCronometro();
+//comecaCronometro();
