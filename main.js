d6b444d93c8d7242d21c7e047fe3f57c0bef9cde
Livia <108752437+liviakat@users.noreply.github.com>
2024-05-09 20:07:23 -0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const dataAtual = newDate();
-let dataObjetivo = prompt("Data final (formato 2024-05-25");
-dataObjetivo = new Date (dataObjetivo+ "T23:59:59");
-let diasQueFaltam = Math.floor ((dataObjetivo - dataAtual) / 86400000);
-document.querySelector("#dias_restante").textContent = diasQueFaltam+ " dias";
